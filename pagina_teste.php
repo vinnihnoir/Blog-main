f7ac4a5696396bd4757ccb946195cfcd48e21b43
vinnihnoir <v.mollina@aluno.ifsp.edu.br>
2025-09-10 12:11:53 -0300

    10/9 #2

---
+++ pagina_teste.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>POST | PROJETO PARA WEB COM PHP</title>
+    <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include 'includes/topo.php';
+                ?>
+            </div>
+        </div>
+        <div class="row" style="min-height: 50px;">
+            <div class="col-md-12">
+                <?php
+                    include 'includes/menu.php';
+                ?>
+            </div>
+            <div class="col-md-10" style="padding-top: 50px;">
+                <h2>PÁGINA DE TESTE INCLUDES</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include 'includes/rodape.php';
+                ?>
+            </div>
+        </div>
+    </div>
+    <p style="padding-left: 250px;">Vinicius de Souza Mollina</p>
+    <script scr="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
